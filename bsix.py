6d8d4c11f323d22f6ce9dfcb43e33f7b817fd530
Ji-bon <68318566+Ji-bon@users.noreply.github.com>
2020-07-16 15:55:18 +0600

    Update bsix.py

--- bsix.py
+++ bsix.py
@@ -1,6 +1,6 @@
 # bsix v1.0
 # Contact Me On Facebook For Using This Tool
-# Coded By Ji-non
+# Coded By jibonkhan
 # If You Wanna Take Credits For This Code, Please Look Yourself Again...
 # Reserved2020
 # Powered by Team VVIRUS
