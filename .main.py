4971d0b0efd8931e854662952d42d5ec2e71bcb4
Ji-bon <68318566+Ji-bon@users.noreply.github.com>
2020-07-16 15:53:12 +0600

    Update .main.py

--- .main.py
+++ .main.py
@@ -1,6 +1,6 @@
 # bsix v1.0
 # Contact Me On Facebook For Using This Tool
-# Coded By BotolMehedi
+# Coded By jibonkhan
 # If You Wanna Take Credits For This Code, Please Look Yourself Again...
 # Reserved2020
 # Powered by Team VVIRUS
